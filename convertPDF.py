5733faa9530d9980a587783f92a9872c8e11567f
unknown <dmow@WS-KIT-5828.brockhome.net>
2016-03-23 13:41:25 -0700

    Implemented tkFileDialog for file selection

--- convertPDF.py
+++ convertPDF.py
@@ -3,6 +3,9 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from cStringIO import StringIO
+#for input file selection dialog
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
 
 import Statement
 import Transaction
@@ -10,47 +13,42 @@ import Sheet
 
 import re
 
-#path variables
-#need better way to get path, user input? / browse to GUI?
-WINDOWS_PATH = "C:\Users\dmow\Documents\CreditTransactionExport\Input\JAN16.pdf"
-OSX_PATH = "/Users/DM/Downloads/creditStatementConversion/Input/NOV14.pdf"
+def getFilePath():
 
-path = WINDOWS_PATH
-
-#Description string has 25 chars max
-#location string has 13 chars max + 2 chars to denote the state/province
-transactionReg = re.compile("(\S.{24})(.{13})(..)( +)(\d+\.\d\d)")
+	#http://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
+	Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+	filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+	return filename
 
 #creates transaction objects given a list of transaction details
 #This should be a Transaction method, or a Statement method?
 def createTransaction(transaction):
 	resultTransaction= Transaction.Transaction(transaction[0].strip(" "), transaction[1] + " " + transaction[2], float(transaction[len(transaction)-1]),  "TestType")
 	return resultTransaction
+	
+def main():
 
-#Statement object
-statementDescription = path.split("\\")
-pdfOutput = Statement.Statement(path, statementDescription[-1].strip(".pdf"))
-#print pdfOutput.pdfString
+	path = getFilePath()
+	
+	#Description string has 25 chars max
+	#location string has 13 chars max + 2 chars to denote the state/province
+	transactionReg = re.compile("(\S.{24})(.{13})(..)( +)(\d+\.\d\d)")
 
-#create transaction objects and append them to Statement Transactions
-for result in transactionReg.findall(pdfOutput.pdfString):
-	pdfOutput.transactions.append(createTransaction(result))
+	#Statement object
+	statementDescription = path.split("/")
+	pdfOutput = Statement.Statement(path, statementDescription[-1].strip(".pdf"))
+	#print pdfOutput.pdfString
 
-#TESTPRINT
-#for transaction in pdfOutput.transactions:
-#	transaction.printTransaction()
+	#create transaction objects and append them to Statement Transactions
+	for result in transactionReg.findall(pdfOutput.pdfString):
+		pdfOutput.transactions.append(createTransaction(result))
 
-#set statement amount
-pdfOutput.calculateTotal()
+	#set statement amount
+	pdfOutput.calculateTotal()
 
-#TESTPRINT
-#pdfOutput.printTransactions()
+	newSheet = Sheet.Sheet(statementDescription[-1].strip(".pdf") + '.xlsx')
+	#sheet template created here too
+	newSheet.recordTransactions(pdfOutput)
 
-#TESTPRINT
-#print to console
-#print pdfOutput.amount
-
-newSheet = Sheet.Sheet(statementDescription[-1].strip(".pdf") + '.xlsx')
-#sheet template created here too
-newSheet.recordTransactions(pdfOutput)
-
+if __name__ == "__main__":
+    main()
